fe43e98a26aabdce06a92c6ddea7f520d49d25d3
WWWCourses <ivawebcourses@gmail.com>
2019-04-05 11:22:49 +0300

    fix: 'newES6syntax' build problem - manual fixes

---
+++ pages/themes/newES6syntax/examples/slides/tmp.js
@@ -0,0 +1,13 @@
+var Rectangle = /** @class */ (function () {
+    function Rectangle(height, width) {
+        this.height = 0;
+        this.;
+        height = height;
+        this.;
+        width = width;
+    }
+    return Rectangle;
+}());
+var r = new Rectangle(2, 3);
+console.log(r.height);
+console.log(r.width);
