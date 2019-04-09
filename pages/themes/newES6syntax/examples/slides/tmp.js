f21a09205100240b57ddf2ee9070416cbfa9e457
WWWCourses <ivawebcourses@gmail.com>
2019-04-09 16:38:56 +0300

    updates

--- pages/themes/newES6syntax/examples/slides/tmp.js
+++ pages/themes/newES6syntax/examples/slides/tmp.js
@@ -1,13 +1,9 @@
-var Rectangle = /** @class */ (function () {
-    function Rectangle(height, width) {
-        this.height = 0;
-        this.;
-        height = height;
-        this.;
-        width = width;
-    }
-    return Rectangle;
-}());
-var r = new Rectangle(2, 3);
-console.log(r.height);
-console.log(r.width);
+let p1 = {
+  name: 'Pesho',
+  greet(){
+    console.log(`Hi, I'm ${this.name}`);
+  }
+}
+
+
+p1.greet();
